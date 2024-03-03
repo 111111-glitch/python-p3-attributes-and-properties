1a352a0cae958c47aff751ac3bd802d26957110b
111111-glitch <ngigimelissa@gmail.com>
2024-03-03 18:45:45 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Mutt", breed="Mutt"):
+        self.name = name
+        if breed in APPROVED_BREEDS or breed == "Mutt":  # Check if the provided breed is in the list of approved breeds or is "Mutt"
+            self.breed = breed
+        else:
+            raise ValueError("Breed must be in list of approved breeds.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+            return 
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS or value == "Mutt":
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from dog import Dog
-
 import io
 import sys
+import pytest
+from dog import Dog
 
 class TestDog:
     '''Dog in dog.py'''
@@ -11,7 +11,7 @@ class TestDog:
     def test_is_class(self):
         '''is a class with the name "Dog".'''
         fido = Dog()
-        assert(type(fido) == Dog)
+        assert type(fido) == Dog
         
     def test_name_not_empty(self):
         '''prints "Name must be string between 1 and 25 characters." if empty string.'''
@@ -19,7 +19,10 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name="")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n"
+
+
+
 
     def test_name_string(self):
         '''prints "Name must be string between 1 and 25 characters." if not string.'''
@@ -27,7 +30,7 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name=123)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert captured_out.getvalue().startswith("Name must be a string between 1 and 25 characters.")
 
     def test_name_under_25(self):
         '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
@@ -35,22 +38,22 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert captured_out.getvalue().startswith("Name must be a string between 1 and 25 characters.")
 
     def test_valid_name(self):
         '''saves name if string between 1 and 25 characters.'''
         fido = Dog(name="Fido")
-        assert(fido.name == "Fido")
+        assert fido.name == "Fido"
 
     def test_breed_not_in_list(self):
         '''prints "Breed must be in list of approved breeds." if not in breed list.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        Dog(breed="Human")
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Breed must be in list of approved breeds.\n")
+        with pytest.raises(ValueError) as e:
+            Dog(breed="Human")
+        assert str(e.value) == "Breed must be in list of approved breeds."
 
     def test_breed_in_list(self):
         '''saves breed if in breed list.'''
         fido = Dog(breed="Pug")
-        assert(fido.breed == "Pug")
+        assert fido.breed == "Pug"
